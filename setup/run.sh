b7e276ddeecc6001de1a2d31effb349bc23a01a2
wian vos <wian@redhat.com>
2019-12-10 11:32:58 +0100

    updated apps

--- setup/run.sh
+++ setup/run.sh
@@ -1,7 +1,8 @@
-oc apply -f 01_argocd_nd.yaml
-oc apply -f 02_argocd.yaml -n 01_argocd
+oc apply -f 01_argocd_ns.yaml
+#oc apply -f 02_argocd.yaml -n argocd
+oc apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v1.3.5/manifests/install.yaml
 sleep 10 
-oc apply -f 03_argocd_route.yaml
+oc apply -f 03_argocd_route.yaml -n argocd 
 
 echo "argocd password:"
 oc get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2
